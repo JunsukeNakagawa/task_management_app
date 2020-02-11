cce7a371827b771e2b2bccd42c486317c94a2645
JunsukeNakagawa <vt250fnx@gmail.com>
2020-02-11 02:43:58 +0000

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#login'
-<<<<<<< HEAD
-  get 'users/new', to: 'users#new'
-  get 'users/edit', to: 'users#edit'
+  resources :users
   get 'tasks/edit', to: 'tasks#edit'
-=======
-
-  get 'users/new', to: 'users#new'
-  get 'users/edit', to: 'users#edit'
-  get 'tasks/edit', to: 'tasks#edit'
-
->>>>>>> 1f89e39851834c3094d3f50fdc7cea18c1609ddc
 end
